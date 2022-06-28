a2dc5f3df7bfb58c9c67bee8d07d8e9284c7ae32
ljh01459796 <ljh01459796@alibaba-inc.com>
2022-06-28 17:08:32 +0800

    init commit.

---
+++ orion-ops-machine-monitor-agent/src/main/java/com/orion/ops/machine/monitor/OrionOpsMachineMonitorAgentApplication.java
@@ -0,0 +1,13 @@
+package com.orion.ops.machine.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionOpsMachineMonitorAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionOpsMachineMonitorAgentApplication.class, args);
+    }
+
+}
---
+++ orion-ops-machine-monitor-agent/src/test/java/com/orion/ops/machine/monitor/OrionOpsMachineMonitorAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.ops.machine.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionOpsMachineMonitorAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
