6428c85de5b0bc1f9f6ac0add5a2dd49b48e53bb
lijiahang <ljh1553488six@139.com>
2022-09-21 18:32:07 +0800

    update install script.

--- orion-ops-plugins-parent/machine-monitor-agent/start-machine-agent.sh
+++ orion-ops-plugins-parent/machine-monitor-agent/start-machine-agent.sh
@@ -4,8 +4,10 @@ pluginPath=$HOME/orion-ops/plugins
 killTag=machine-monitor-agent
 logPath=$pluginPath/machine-monitor-agent.out
 scriptPath=$pluginPath/start-machine-monitor-agent.sh
-mkdir -p ${pluginPath}
-echo "ps -ef | grep ${killTag} | grep -v grep | awk '{print \$2}' | xargs kill -9 || echo \$?
-nohup java -jar ${agentJarPath} --machineId=${machineId} --spring.profiles.active=prod >> ${logPath} &" > ${scriptPath}
+echo "eof=$\"\n\"
+ps -ef | grep ${killTag} | grep -v grep | awk '{print \$2}' | xargs kill -9 || echo \$?
+nohup java -jar ${agentJarPath} --machineId=${machineId} --spring.profiles.active=prod 2>&1 >> ${logPath} &
+eof=\$(echo -e \$eof)
+echo \$eof" > ${scriptPath}
 chmod 777 ${scriptPath}
 ${scriptPath}
